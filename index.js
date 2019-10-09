f911f939b664555dc4a32d4af20205e35e0e46de
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-09 17:45:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -10,6 +12,10 @@ function renderBooks(json) {
     main.appendChild(h2)
   })
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
